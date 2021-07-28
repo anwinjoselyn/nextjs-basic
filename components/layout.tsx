6d08aae9e563336235f206925dc6fd1272df10ce
Anwin Joselyn <anwin.joselyn@clickpost.in>
2021-07-28 16:53:42 +0530

    moved to Typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,13 @@ import Link from 'next/link';
 const name = 'Anwin Joselyn';
 export const siteTitle = 'Anwin Joselyn blog';
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
